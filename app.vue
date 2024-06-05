0f97524ace8cefe523e234df44fb18e5bff06070
yusufgshehu <yusufgshehu@gmail.com>
2024-06-05 18:59:11 +0000

    Home Page
    made some improvements to responsiveness :thumbs-up

--- app.vue
+++ app.vue
@@ -127,29 +127,156 @@ const bakery_menu_items: BakeryMenuItem[] = [
 </script>
 
 <template>
-  <nav class="flex flex-row justify-between p-10 bg-primary-500 sticky border-b-2 border-primary-500 rounded-b-lg"> 
-<div class="space-x-5">
+  <nav class=" md:flex flex-row text-center justify-center md:justify-between p-10 bg-background md:bg-primary-500 sticky md:border-b-2 border-primary-500 rounded-b-lg"> 
+<div class="space-x-5 hidden">
   <NuxtLink>Home</NuxtLink>
   <NuxtLink>Menu</NuxtLink>
   <NuxtLink>History</NuxtLink>
 </div>
 
 <div>
-  <h1 class="text-2xl font-bold">
+  <h1 class="text-4xl md:text-2xl font-bold">
 
     BreadTopia
   </h1>
 </div>
-<div class="space-x-5">
+<div class="space-x-5 hidden">
   <NuxtLink>My Cart</NuxtLink>
   <NuxtLink>Contact</NuxtLink>
 </div>
 
   </nav>
+
+  <nav
+      style="position: fixed; bottom: 0"
+      class="md:hidden mb-5 w-full z-50 flex p-4 justify-center bg-primary-500 rounded-full shadow-lg"
+    >
+      <div
+        class="
+          text-sm
+          drop-shadow-2xl
+          py-4
+          flex
+          justify-evenly
+          rounded-2xl
+          space-x-2
+          w-full
+          text-center
+          bg-cultured
+          dark:bg-oxford_blue dark:text-cultured
+        "
+      >
+        <div class="text-center">
+          <nuxt-link to="/" class="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 m-auto"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819"
+              />
+            </svg>
+            <span> Home </span>
+          </nuxt-link>
+        </div>
+
+        <div class="text-center">
+          <nuxt-link to="/about" class="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 m-auto"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+              />
+            </svg>
+
+            <span> About </span>
+          </nuxt-link>
+        </div>
+        <div class="text-center">
+          <nuxt-link to="/projects" class="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 m-auto"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"
+              />
+            </svg>
+
+            <span> Projects </span>
+          </nuxt-link>
+        </div>
+        <div class="text-center">
+          <a
+            href="https://ys-portfolio-site.s3.eu-west-1.amazonaws.com/Resume.pdf"
+            target="_blank"
+            class=""
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 m-auto"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+              />
+            </svg>
+
+            <span> Resume </span>
+          </a>
+        </div>
+
+        <div class="text-center">
+          <a href="mailto:yusufgshehu@gmail.com" class="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 m-auto"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"
+              />
+            </svg>
+
+            <span> Mail Me </span>
+          </a>
+        </div>
+      </div>
+    </nav>
   <div class="">
 
     <div class="relative isolate px-6  lg:px-8 bg-background">
-      <img src="/img/bread.png" class=" absolute" alt="Discover Nuxt 3" />
+      <img src="/img/bread.png" class="hidden md:flex absolute" alt="Discover Nuxt 3" />
       <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div class=" flex flex-col items-center justify-center text-center px-5">
           
@@ -175,22 +302,22 @@ const bakery_menu_items: BakeryMenuItem[] = [
     <!-- <NuxtWelcome /> -->
 
 
-    <div class="flex flex-row justify-between p-10 bg-background">
-      <div class="flex flex-col w-1/2 space-y-3"> <div class="boorder border-primary-500 border-2 rounded-lg  w-20 dropshadow-lg shadow-lg"></div>
+    <div class="flex flex-row flex-wrap-reverse justify-between p-10 bg-background">
+      <div class="flex flex-col w-full md:w-1/2 space-y-3"> <div class="boorder border-primary-500 border-2 rounded-lg  w-20 dropshadow-lg shadow-lg"></div>
 
         <p class="text-sm font-semibold ">
           Experimenta la tradición y el amor horneados en cada hogaza
         </p>
 
-        <h2 class="text-primary-700 sm:text-6xl line-height-loose">
+        <h2 class="text-primary-700 text-6xl line-height-loose">
           Feel the warm embrace of our homemade bread.
         </h2>
         <p class="font-semibold ">
           Experience the tradition passed down through generations, baked with love in every loaf of our homemade bread. Enjoy the crusty exterior that gives way to a fluffy and flavorful interior. It's not just bread, it's a little piece of home.
         </p>
       </div>
-      <div class="flex-col w-1/2">
-        <img src="/img/cookies.png" class="w-full " alt="Discover Nuxt 3" />
+      <div class="flex-col w-full text-center justify-center mx-auto md:w-1/2">
+        <img src="/img/cookies.png" class="w-1/3 md:w-full text-center mx-auto justify-center" alt="Discover Nuxt 3" />
 
        
       </div>
@@ -205,7 +332,7 @@ const bakery_menu_items: BakeryMenuItem[] = [
       </div>
 
       <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-        <div v-for="(n, index) in 4">
+        <div v-for="(n, index) in 4" >
           <MenuItemCard :item="bakery_menu_items[index]" />
         </div>
 
@@ -220,7 +347,7 @@ const bakery_menu_items: BakeryMenuItem[] = [
 
         <h2 class="text-5xl font-bold text-primary-700">Más que pan</h2>
 
-        <div class="w-1/2 mx-auto">
+        <div class="md:w-1/2 md:mx-auto">
 
           <p class="text-lg font-semibold ">our homemade bread is a unique sensory experience. From the moment you inhale the aroma of yeast and freshly baked flour, to the comforting feeling of warmth in your hands and the homemade flavor that fills your palate, you'll feel right at home. Enjoy the simplicity and goodness of natural ingredients combined with the love and passion for baking</p>
         </div>
@@ -229,12 +356,12 @@ const bakery_menu_items: BakeryMenuItem[] = [
       </div>
 
 
-      <div class="p-24 bg-background">
-        <div class="flex flex-row justify-center space-x-6 p-10 bg-primary-300  shadow-md shadow rounded-md">
-          <div class="flex flex-col">
+      <div class="md:p-24 bg-background py-10">
+        <div class="flex flex-row flex-wrap md:flex-no-wrap justify-center space-x-6 p-10 bg-primary-300 md:space-y-0 space-y-9 shadow-md shadow rounded-md">
+          <div class="flex flex-col  w-full md:w-1/2">
             <img src="/img/store.png" class="w-full" alt="bread line" />
              </div>
-          <div class="flex flex-col justify-between align-items-center h-2/3 my-auto space-y-10">
+          <div class="flex flex-col justify-between align-items-center h-2/3 my-auto md:space-y-10">
           <h1 class="text-4xl"> Contact Us While it’s Still Hot!!!</h1>
 
           <div>
